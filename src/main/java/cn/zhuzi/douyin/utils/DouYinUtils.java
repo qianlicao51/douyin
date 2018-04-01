689efa69b57da9d55c5a3821e94e8083c1c06fa0
千里草51 <34734081+qianlicao51@users.noreply.github.com>
2018-04-01 22:30:28 +0800

    Update DouYinUtils.java

--- src/main/java/cn/zhuzi/douyin/utils/DouYinUtils.java
+++ src/main/java/cn/zhuzi/douyin/utils/DouYinUtils.java
@@ -7,6 +7,7 @@ public class DouYinUtils {
 	public static void sleepForColl(Integer sleepTime) {
 		try {
 			Thread.sleep(sleepTime);
+			
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
